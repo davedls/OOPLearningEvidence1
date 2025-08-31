6ca0c6c4677bc6d3df1d12daf0484e5c763b638d
Kenneth_Ciriaco <crownedtable@gmail.com>
2025-08-31 21:20:01 +0800

    Update Main.java

--- oop/Main.java
+++ oop/Main.java
@@ -5,24 +5,32 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         System.out.print("|1}   Ferry\n|2}   Airplane\nMeans of Travel: ");
-        int TransportMode = Integer.parseInt(scan.nextLine());
-        System.out.println("\n--------------------\n");
-        if(TransportMode == 1){
-            PlaneTicket flight = new PlaneTicket();
-            flight.setFlightInfo();
-            flight.displayPersonalInfo();
-            flight.displayTravelDetails();
-            flight.displayTicketDetails();
-        } else if(TransportMode == 2){
-            FerryTicket cruise = new FerryTicket();
-            cruise.setFerryInfo();
-            cruise.displayPersonalInfo();
-            cruise.displayTravelDetails();
-            cruise.displayTicketDetails();
+        while(true){      
+            try {
+                while(true){
+                    int TransportMode = Integer.parseInt(scan.nextLine());
+                    if(TransportMode == 1){
+                        PlaneTicket flight = new PlaneTicket();
+                        flight.setFlightInfo();
+                        flight.displayPersonalInfo();
+                        flight.displayTravelDetails();
+                        flight.displayTicketDetails();
+                        break;
+                    } else if(TransportMode == 2){
+                        FerryTicket cruise = new FerryTicket();
+                        cruise.setFerryInfo();
+                        cruise.displayPersonalInfo();
+                        cruise.displayTravelDetails();
+                        cruise.displayTicketDetails();
+                        break;
+                    } else {
+                        System.out.println("Number not found");
+                        continue;
+                    }
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter numbers only.");
+            }
         }
-        
-        
-        
-        
     }
 }
