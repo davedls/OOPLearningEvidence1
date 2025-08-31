56755b68e2d02aef71771b7a92e02b665de753e6
Kenneth_Ciriaco <crownedtable@gmail.com>
2025-08-31 23:32:28 +0800

    Update Main.java

--- oop/Main.java
+++ oop/Main.java
@@ -3,34 +3,45 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-
-        System.out.print("|1}   Ferry\n|2}   Airplane\nMeans of Travel: ");
-        while(true){      
-            try {
-                while(true){
-                    int TransportMode = Integer.parseInt(scan.nextLine());
-                    if(TransportMode == 1){
-                        PlaneTicket flight = new PlaneTicket();
-                        flight.setFlightInfo();
-                        flight.displayPersonalInfo();
-                        flight.displayTravelDetails();
-                        flight.displayTicketDetails();
-                        break;
-                    } else if(TransportMode == 2){
-                        FerryTicket cruise = new FerryTicket();
-                        cruise.setFerryInfo();
-                        cruise.displayPersonalInfo();
-                        cruise.displayTravelDetails();
-                        cruise.displayTicketDetails();
-                        break;
-                    } else {
-                        System.out.println("Number not found");
-                        continue;
+        while(true){  
+            System.out.println("\n|Transportation Method\n|---------------------\n|1}   Ferry\n|2}   Airplane\n|3}   Train\n|---------------------");
+            while(true){      
+                try {
+                    while(true){
+                        System.out.print("|Means of Travel: ");
+                        int TransportMode = Integer.parseInt(scan.nextLine());
+                        if(TransportMode == 1){
+                            PlaneTicket flight = new PlaneTicket();
+                            flight.setFlightInfo();
+                            flight.displayPersonalInfo();
+                            flight.displayTravelDetails();
+                            flight.displayTicketDetails();
+                            break;
+                        } else if(TransportMode == 2){
+                            FerryTicket cruise = new FerryTicket();
+                            cruise.setFerryInfo();
+                            cruise.displayPersonalInfo();
+                            cruise.displayTravelDetails();
+                            cruise.displayTicketDetails();
+                            break;
+                        } else if(TransportMode == 3){
+                            TrainTicket Rails = new TrainTicket();
+                            Rails.setTrainInfo();
+                            Rails.displayPersonalInfo();
+                            Rails.displayTravelDetails();
+                            Rails.displayTicketDetails();
+                            break;
+                        } else {
+                            System.out.println("Number not found");
+                            continue;
+                        }
                     }
+                } catch (NumberFormatException e) {
+                    System.out.println("Please enter numbers only.");
                 }
-            } catch (NumberFormatException e) {
-                System.out.println("Please enter numbers only.");
+                break;
             }
+
         }
     }
 }
